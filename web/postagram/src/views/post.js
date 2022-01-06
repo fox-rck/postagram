f89031ef0b5432ddb605e482c1c8e1532c5d1f0b
RF <rfox@apples-MacBook-Pro.local>
2022-01-06 16:44:22 -0600

    working on permissions of a post and the styling of the component

--- web/postagram/src/views/post.js
+++ web/postagram/src/views/post.js
@@ -4,17 +4,40 @@
 / Post view for showing all blogs
 */
 
-
-import { Component } from 'react'
+import { Component, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-const Post = ({...props}) => {
+import api from "../services/api";
+import PostElm from "../components/post";
 
-	const {id} = useParams()
-	return (<>
-		{'Post'}{id}
-		</>
-	)
-}
+const Post = ({ ...props }) => {
+	// pull the id from the url params
+	const { id } = useParams();
+	const [loadingPost, setLoading] = useState(1);
+	const [post, setPost] = useState(null);
+
+	useEffect(async () => {
+		// call everytime we get a new post id
+		try {
+			setLoading(1);
+			let postMod = await api.getPostById(id);
+			// console.log("postMod", postMod.post);
+			if (postMod && postMod.post) {
+				setPost(postMod.post);
+				setLoading(0);
+			}
+		} catch (e) {
+			console.log("error", e);
+		}
+	}, [id]);
+
+	return (
+		<div className="inner-page bg-white">
+			{!loadingPost ? (
+				post ? <PostElm post={post} /> : null
+			) : null}
+		</div>
+	);
+};
 
 export default Post;
