fac63244b9b98e937f6348760c23af8cdc6ab92a
RF <rfox@apples-MacBook-Pro.local>
2022-01-06 00:43:00 -0600

    update to pools for code clean up

--- server/connectors/pool.js
+++ server/connectors/pool.js
@@ -22,47 +22,21 @@ const pool = new Pool({
     // dateStrings: true,
 })
 
-pool.on('query', (sql) => {
-  console.error('sql', sql)
-})
-
 // the pool will emit an error on behalf of any idle clients
 // it contains if a backend error or network partition happens
 pool.on('error', (err, client) => {
   console.error('Unexpected error on idle client', err)
   process.exit(-1)
 })
-// var pool  = mysql.createPool({
-//     database: process.env.DB_DATABASE,
-//     host: process.env.DB_HOST,
-// 	port: process.env.DB_PORT,
-//     user: process.env.DB_USER,
-// 	password: process.env.DB_PASSWORD,
-//     connectionLimit : 4,
-//     connectTimeout: 30000,
-//     charset : 'utf8mb4',
-//     timezone: 'UTC+0',
-//     dateStrings: true,
-// });
 
 function getConnection() {
     return new Promise(function (resolve, reject) {
-      // pool.getConnection(function (err, connection) {
-      //   // connected! (unless `err` is set)
-      //   if (err) {
-      //     console.log(err);
-      //     reject();
-      //   } else {
-      //     resolve(connection);
-      //   }
-      // });
-      // callback - checkout a client
+        // get a client from the pool
         pool.connect((err, client, done) => {
           if (err) {
             return reject()
           }
           resolve(client, done)
-          
         })
     });
 }
