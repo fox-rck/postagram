c9b75f726539478252a67291f760804ab81f7293
RF <rfox@apples-MacBook-Pro.local>
2022-01-07 16:28:47 -0600

    removal of unused file

--- web/postagram/src/app.test.js
+++
@@ -1,8 +0,0 @@
-import { render, fireEvent, waitFor, screen } from "@testing-library/react";
-import App from "./app";
-
-test("renders", () => {
-  // render(<App />);
-  // const logo = screen.ByAltText('Postagram');
-  // expect(logo).toBeInTheDocument();
-});
